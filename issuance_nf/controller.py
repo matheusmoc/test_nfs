64b78c684d5ba181c995e6732651a672aef969b6
matheusmoc <matheusandrade009@gmail.com>
2024-05-14 16:29:08 -0300

    FIXTURE: update and create class controller

--- issuance_nf/controller.py
+++ issuance_nf/controller.py
@@ -5,16 +5,21 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from .models import Emissor
 
-class Issuance(View):
+
+class IssuanceViewIndex(View):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
-        return super(Issuance, self).dispatch(request, *args, **kwargs)
+        return super(IssuanceViewIndex, self).dispatch(request, *args, **kwargs)
 
     def get(self, request, pk=None):
         data = Emissor.objects.all()
         context = {'data': data}
         return render(request, 'index.html', context=context)
 
+class IssuanceViewIndexCreate(View):
+    def get(self, request):
+        return render(request, 'create.html')
+    
     def post(self, request):
         nome = request.POST.get('nome')
         email = request.POST.get('email')
@@ -22,13 +27,19 @@ class Issuance(View):
         data_emissao = request.POST.get('data_emissao')
 
         emissor = Emissor.objects.create(nome=nome, email=email, telefone=telefone, data_emissao=data_emissao)
-        return redirect('nfs')
+        return redirect('nfs_detail', pk=emissor.pk)
 
+
+class IssuanceViewIndexDelete(View):
     def delete(self, request, pk):
         emissor = get_object_or_404(Emissor, pk=pk)
         emissor.delete()
         return HttpResponse("Emissor deletado com sucesso!")
 
+class IssuanceViewIndexUpdate(View):
+    def get(self, request):
+        return render(request, 'update.html')
+    
     def put(self, request, pk):
         emissor = get_object_or_404(Emissor, pk=pk)
         emissor.nome = request.POST.get('nome')
--- issuance_nf/urls.py
+++ issuance_nf/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from. import controller
 
 urlpatterns = [
-    path('', controller.Issuance.as_view(), name='index'),
-    path('create/', controller.Issuance.as_view(), name='create'),
-    path('delete/<int:pk>/', controller.Issuance.as_view(), name='delete'),
-    path('update/<int:pk>/', controller.Issuance.as_view(), name='update'),
+    path('', controller.IssuanceViewIndex.as_view(), name='index'),
+    path('create/', controller.IssuanceViewIndexCreate.as_view(), name='create'),
+    path('delete/<int:pk>/', controller.IssuanceViewIndexDelete.as_view(), name='delete'),
+    path('update/<int:pk>/', controller.IssuanceViewIndexUpdate.as_view(), name='update'),
 ]

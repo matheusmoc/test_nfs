cebe228a104d5d1ae759e78ed7e8ce8c774535e3
matheusmoc <matheusandrade009@gmail.com>
2024-05-14 23:11:14 -0300

    FIXTURE: delete method

--- issuance_nf/controller.py
+++ issuance_nf/controller.py
@@ -3,20 +3,21 @@ from django.http import HttpResponse
 from django.views import View
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
-from .models import Emissor
+from .models import NotaFiscal
+from django.http import HttpResponseBadRequest
 
 
-class IssuanceViewIndex(View):
+class IssuanceIndexView(View):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
-        return super(IssuanceViewIndex, self).dispatch(request, *args, **kwargs)
+        return super(IssuanceIndexView, self).dispatch(request, *args, **kwargs)
 
     def get(self, request, pk=None):
-        data = Emissor.objects.all()
+        data = NotaFiscal.objects.all()
         context = {'data': data}
         return render(request, 'index.html', context=context)
 
-class IssuanceViewIndexCreate(View):
+class IssuanceCreateView(View):
     def get(self, request):
         return render(request, 'create.html')
     
@@ -26,25 +27,26 @@ class IssuanceViewIndexCreate(View):
         telefone = request.POST.get('telefone')
         data_emissao = request.POST.get('data_emissao')
 
-        emissor = Emissor.objects.create(nome=nome, email=email, telefone=telefone, data_emissao=data_emissao)
-        return redirect('nfs_detail', pk=emissor.pk)
+        nota_fiscal = NotaFiscal.objects.create(nome=nome, email=email, telefone=telefone, data_emissao=data_emissao)
+        return redirect('nfs_detail', pk=nota_fiscal.pk)
 
 
-class IssuanceViewIndexDelete(View):
-    def delete(self, request, pk):
-        emissor = get_object_or_404(Emissor, pk=pk)
-        emissor.delete()
-        return HttpResponse("Emissor deletado com sucesso!")
-
-class IssuanceViewIndexUpdate(View):
+class IssuanceDeleteView(View):
+    def post(self, request, *args, **kwargs):
+        pk = request.POST.get('pk')
+        nota_fiscal = NotaFiscal.objects.all()
+        nota_fiscal.delete()
+        return redirect('/nfs')
+        
+class IssuanceUpdateView(View):
     def get(self, request):
         return render(request, 'update.html')
     
     def put(self, request, pk):
-        emissor = get_object_or_404(Emissor, pk=pk)
-        emissor.nome = request.POST.get('nome')
-        emissor.email = request.POST.get('email')
-        emissor.telefone = request.POST.get('telefone')
-        emissor.data_emissao = request.POST.get('data_emissao')
-        emissor.save()
+        nota_fiscal = get_object_or_404(NotaFiscal, pk=pk)
+        nota_fiscal.nome = request.POST.get('nome')
+        nota_fiscal.email = request.POST.get('email')
+        nota_fiscal.telefone = request.POST.get('telefone')
+        nota_fiscal.data_emissao = request.POST.get('data_emissao')
+        nota_fiscal.save()
         return HttpResponse("Emissor atualizado com sucesso!")
